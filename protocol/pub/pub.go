36f30212c6421fbb5de37cc4825a80be5cac4d99
Tyler Treat <ttreat31@gmail.com>
2014-12-17 18:41:36 -0600

    Buffer the publisher endpoint channel
    This solves the problem described here: https://gist.github.com/tylertreat/72dd72a5b2ed0251443f
    The problem is publisher messages are dropped because of artificial backpressure due to the
    use of an unbuffered channel. Adding a small buffer alleviates the problem.

--- protocol/pub/pub.go
+++ protocol/pub/pub.go
@@ -83,7 +83,7 @@ func (p *pub) sender() {
 }
 
 func (p *pub) AddEndpoint(ep mangos.Endpoint) {
-	pe := &pubEp{ep: ep, sock: p.sock, q: make(chan *mangos.Message)}
+	pe := &pubEp{ep: ep, sock: p.sock, q: make(chan *mangos.Message, 5)}
 	p.Lock()
 	p.eps[ep.GetID()] = pe
 	p.Unlock()
