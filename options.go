d2ba4c896cdc2bb07ba8728e9539ae4d38668381
Tyler Treat <ttreat31@gmail.com>
2015-01-23 19:42:28 -0600

    Added options for setting uwq/urq sizes
    New options OptionWriteQLen and OptionReadQLen let you set the
    respective queue lengths before calling Dial/Listen. Supports both
    SetOption and GetOption.

--- options.go
+++ options.go
@@ -83,4 +83,14 @@ const (
 	// called because it is provided, but this option is useful in the event
 	// that the port is assigned by the OS (i.e. port "0").
 	OptionLocalAddress = "LOCAL-ADDRESS"
+
+	// OptionWriteQLen is used to set the size, in messages, of the write queue
+	// channel. By default, it's 10. This option cannot be set if Dial or
+	// Listen has been called on the socket.
+	OptionWriteQLen = "WRITEQ-LEN"
+
+	// OptionReadQLen is used to set the size, in messages, of the read queue
+	// channel. By default, it's 10. This option cannot be set if Dial or
+	// Listen has been called on the socket.
+	OptionReadQLen = "READQ-LEN"
 )
