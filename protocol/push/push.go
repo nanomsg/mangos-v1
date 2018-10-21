f87e8e5d99a46d42d32bbb862973a2af7ddcb9f8
Garrett D'Amore <garrett@damore.org>
2018-10-21 12:39:06 -0700

    Refactored PUSH to use new protocol API.
    This also now provides guaranteed round-robin balancing, and
    sets the stage for receive priorities.

--- protocol/push/push.go
+++ protocol/push/push.go
@@ -17,116 +17,36 @@
 package push
 
 import (
-	"sync"
-	"time"
-
-	"nanomsg.org/go/mangos/v2"
+	"nanomsg.org/go/mangos/v2/protocol"
+	"nanomsg.org/go/mangos/v2/protocol/xpush"
 )
 
-type push struct {
-	sock mangos.ProtocolSocket
-	raw  bool
-	w    mangos.Waiter
-	eps  map[uint32]*pushEp
-	sync.Mutex
+type socket struct {
+	protocol.Protocol
 }
 
-type pushEp struct {
-	ep mangos.Endpoint
-	cq chan struct{}
-}
-
-func (x *push) Init(sock mangos.ProtocolSocket) {
-	x.sock = sock
-	x.w.Init()
-	x.sock.SetRecvError(mangos.ErrProtoOp)
-	x.eps = make(map[uint32]*pushEp)
-}
-
-func (x *push) Shutdown(expire time.Time) {
-	x.w.WaitAbsTimeout(expire)
-}
-
-func (x *push) sender(ep *pushEp) {
-	defer x.w.Done()
-	sq := x.sock.SendChannel()
-	cq := x.sock.CloseChannel()
-
-	for {
-		select {
-		case <-cq:
-			return
-		case <-ep.cq:
-			return
-		case m := <-sq:
-			if m == nil {
-				sq = x.sock.SendChannel()
-				continue
-			}
-			if ep.ep.SendMsg(m) != nil {
-				m.Free()
-				return
-			}
-		}
-	}
-}
-
-func (*push) Number() uint16 {
-	return mangos.ProtoPush
-}
-
-func (*push) PeerNumber() uint16 {
-	return mangos.ProtoPull
-}
-
-func (*push) Name() string {
-	return "push"
-}
-
-func (*push) PeerName() string {
-	return "pull"
-}
-
-func (x *push) AddEndpoint(ep mangos.Endpoint) {
-	pe := &pushEp{ep: ep, cq: make(chan struct{})}
-	x.Lock()
-	x.eps[ep.GetID()] = pe
-	x.Unlock()
-	x.w.Add()
-	go x.sender(pe)
-	go mangos.NullRecv(ep)
-}
-
-func (x *push) RemoveEndpoint(ep mangos.Endpoint) {
-	id := ep.GetID()
-	x.Lock()
-	pe := x.eps[id]
-	delete(x.eps, id)
-	x.Unlock()
-	if pe != nil {
-		close(pe.cq)
-	}
-}
-
-func (x *push) SetOption(name string, v interface{}) error {
-	return mangos.ErrBadOption
-}
-
-func (x *push) GetOption(name string) (interface{}, error) {
+func (s *socket) GetOption(name string) (interface{}, error) {
 	switch name {
-	case mangos.OptionRaw:
-		return x.raw, nil
-	default:
-		return nil, mangos.ErrBadOption
+	case protocol.OptionRaw:
+		return false, nil
 	}
+	return s.Protocol.GetOption(name)
 }
 
-// NewSocket allocates a new Socket using the PUSH protocol.
-func NewSocket() (mangos.Socket, error) {
-	return mangos.MakeSocket(&push{raw: false}), nil
+// Info returns protocol information.
+func Info() protocol.Info {
+	return xpush.Info()
 }
 
-// NewRawSocket allocates a raw Socket using the PUSH protocol.
-func NewRawSocket() (mangos.Socket, error) {
-	return mangos.MakeSocket(&push{raw: true}), nil
+// NewProtocol returns a new protocol implementation.
+func NewProtocol() protocol.Protocol {
+	s := &socket{
+		Protocol: xpush.NewProtocol(),
+	}
+	return s
+}
+
+// NewSocket allocates a raw Socket using the PULL protocol.
+func NewSocket() (protocol.Socket, error) {
+	return protocol.MakeSocket(NewProtocol()), nil
 }
