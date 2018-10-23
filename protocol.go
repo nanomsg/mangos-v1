8eb9b9822b20916031b955c5d7543993503de114
Garrett D'Amore <garrett@damore.org>
2018-10-22 22:30:13 -0400

    Yank the legacy Protocol definitions.

--- protocol.go
+++ protocol.go
@@ -14,10 +14,6 @@
 
 package mangos
 
-import (
-	"time"
-)
-
 //
 // LEGACY PROTOCOL STUFF
 //
@@ -40,116 +36,6 @@ type Endpoint interface {
 	// RecvMsg receives a message.  It blocks until the message is
 	// received.  On error, the pipe is closed and nil is returned.
 	RecvMsg() *Message
-}
-
-// Protocol implementations handle the "meat" of protocol processing.  Each
-// protocol type will implement one of these.  For protocol pairs (REP/REQ),
-// there will be one for each half of the protocol.
-type Protocol interface {
-
-	// Init is called by the core to allow the protocol to perform
-	// any initialization steps it needs.  It should save the handle
-	// for future use, as well.
-	Init(ProtocolSocket)
-
-	// Shutdown is used to drain the send side.  It is only ever called
-	// when the socket is being shutdown cleanly. Protocols should use
-	// the linger time, and wait up to that time for sockets to drain.
-	Shutdown(time.Time)
-
-	// AddEndpoint is called when a new Endpoint is added to the socket.
-	// Typically this is as a result of connect or accept completing.
-	AddEndpoint(Endpoint)
-
-	// RemoveEndpoint is called when an Endpoint is removed from the socket.
-	// Typically this indicates a disconnected or closed connection.
-	RemoveEndpoint(Endpoint)
-
-	// ProtocolNumber returns a 16-bit value for the protocol number,
-	// as assigned by the SP governing body. (IANA?)
-	Number() uint16
-
-	// Name returns our name.
-	Name() string
-
-	// PeerNumber() returns a 16-bit number for our peer protocol.
-	PeerNumber() uint16
-
-	// PeerName() returns the name of our peer protocol.
-	PeerName() string
-
-	// GetOption is used to retrieve the current value of an option.
-	// If the protocol doesn't recognize the option, EBadOption should
-	// be returned.
-	GetOption(string) (interface{}, error)
-
-	// SetOption is used to set an option.  EBadOption is returned if
-	// the option name is not recognized, EBadValue if the value is
-	// invalid.
-	SetOption(string, interface{}) error
-}
-
-// ProtocolSocket is the "handle" given to protocols to interface with the
-// socket.  The Protocol implementation should not access any sockets or pipes
-// except by using functions made available on the ProtocolSocket.  Note
-// that all functions listed here are non-blocking.
-type ProtocolSocket interface {
-	// SendChannel represents the channel used to send messages.  The
-	// application injects messages to it, and the protocol consumes
-	// messages from it.  The channel may be closed when the core needs to
-	// create a new channel, typically after an option is set that requires
-	// the channel to be reconfigured.  (OptionWriteQLen) When the protocol
-	// implementation notices this, it should call this function again to obtain
-	// the value of the new channel.
-	SendChannel() <-chan *Message
-
-	// RecvChannel is the channel used to receive messages.  The protocol
-	// should inject messages to it, and the application will consume them
-	// later.
-	RecvChannel() chan<- *Message
-
-	// The protocol can wait on this channel to close.  When it is closed,
-	// it indicates that the application has closed the upper read socket,
-	// and the protocol should stop any further read operations on this
-	// instance.
-	CloseChannel() <-chan struct{}
-
-	// GetOption may be used by the protocol to retrieve an option from
-	// the socket.  This can ultimately wind up calling into the socket's
-	// own GetOption handler, so care should be used!
-	GetOption(string) (interface{}, error)
-
-	// SetOption is used by the Protocol to set an option on the socket.
-	// Note that this may set transport options, or even call back down
-	// into the protocol's own SetOption interface!
-	SetOption(string, interface{}) error
-
-	// SetRecvError is used to cause socket RX callers to report an
-	// error.  This can be used to force an error return rather than
-	// waiting for a message that will never arrive (e.g. due to state).
-	// If set to nil, then RX works normally.
-	SetRecvError(error)
-
-	// SetSendError is used to cause socket TX callers to report an
-	// error.  This can be used to force an error return rather than
-	// waiting to send a message that will never be delivered (e.g. due
-	// to incorrect state.)  If set to nil, then TX works normally.
-	SetSendError(error)
-}
-
-// NullRecv simply loops, receiving and discarding messages, until the
-// Endpoint returns back a nil message.  This allows the Endpoint to notice
-// a dropped connection.  It is intended for use by Protocols that are write
-// only -- it lets them become aware of a loss of connectivity even when they
-// have no data to send.
-func NullRecv(ep Endpoint) {
-	for {
-		var m *Message
-		if m = ep.RecvMsg(); m == nil {
-			return
-		}
-		m.Free()
-	}
 }
 
 //
